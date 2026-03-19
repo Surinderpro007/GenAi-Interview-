be091ba228d4cdca8ad3c006968ff05aae4b6605
Surinder Singh <surindersinghpro@proton.me>
2026-03-19 17:44:44 +0530

    add Railway backend link

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-     baseURL: 'https://genai-interview-production.up.railway.app/',
+     baseURL: 'genai-interview-production.up.railway.app',
      withCredentials:true,
 })
 
